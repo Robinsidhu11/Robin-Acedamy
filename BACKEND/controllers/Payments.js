b5b571212e4ad3f3f3dd0d998f8c6ed66f667568
Robinsidhu11 <rbnsidhu11@gmail.com>
2023-06-23 12:05:34 +0530

    verify payment controller done

--- BACKEND/controllers/Payments.js
+++ BACKEND/controllers/Payments.js
@@ -3,9 +3,9 @@ const {instance}=require('../config/razorpay')
 const User=require('../models/User')
 const Course=require('../models/Course')
 
-const mailsender=require('../utils/mailSender')
 const courseEnrollmentEmail=require('../mailtemplates/courseEnrollmentEmail')
 const { default: mongoose } = require('mongoose')
+const mailSender = require('../utils/mailSender')
 
 //controller to validate user and course, create & initiate payment.
 exports.capturePayment=async (req,res)=>{
@@ -98,3 +98,63 @@ exports.capturePayment=async (req,res)=>{
 }
 
 //controller to verify payment
+//verify signature secret of razorpay and server
+//remember this controller will be initiated by razorpay not our request
+exports.verifySignature=async (req,res)=>{
+    //our intend is, we getting a our key in encrypted form from the razorpay (called signature), we have secret stored at out server too. called webhookSecret. so we cant decrypt that signature and match with webhook secret rather we can do is encrypt our webhooksecret using same encrypt algos that razorpay used to encrypt secret.
+    //and then we can match our encrypted thing to the signature received from webhook
+
+    //say webhook (secret we have given earlier to razorpay)
+    const webhookSecret="12345678"
+
+    
+    //fetching signature sent from razorpay
+    const signature=req.headers("x-razorpay-signature")
+
+    //first param is algo, second is secret
+    const shasum=crypto.createHmac("sha256",webhookSecret)
+    shasum.update(JSON.stringify(req.body))
+    const digest=shasum.digest("hex")
+
+    //matching signature and our encrypted key
+    if(signature==digest){
+        console.log("payment is authorized")
+        //as this controller was called by RAZORPAY SO WE CANT NOW ACCESS THE COURSE AND SUER ID FROM REQ BODY. BUT WE DO PASSED THOSE IN OPTIONS WHILE CREATING Order. so we can acess through that
+        const {courseId,userid}=req.body.payload.payment.entity.notes;
+
+        //we need to update course in users array and also in course model inside student enrolled we need to update there too
+    try{
+        const updatedCourseDetails=await Course.findByIdAndUpdate({_id:courseId},{$push:{studentsEnrolled:userid}},{new:true})
+        if(!updatedCourseDetails){
+            return res.status(400).json({
+                success:false,
+                message:"course not found"
+            })
+        }
+        
+        //find user and update there too
+        const updatedUserDetails=await User.findByIdAndUpdate({_id:userid},{$push:{courses:courseId}},{new:true})
+
+        //mailsend for cnfirmation to user
+        //TDOD: ATTACH TEMPLATE TOO FOR COURSE ENROLLMENT
+        const emailResponse=await mailSender(updatedUserDetails.email,"Congrats enrolled","Congratulations you are on boarded into new course")
+        
+        return res.status(200).json({
+            success:true,
+            message:"Signature verified and Course purchased sucessfully"
+        })
+        }
+    catch(err){
+            return res.status(500).json({
+                success:false,
+                message:err.message
+            })
+        }
+    }
+    else{
+        return res.status(400).json({
+            success:false,
+            message:"invalid request"
+        })
+    }
+}
