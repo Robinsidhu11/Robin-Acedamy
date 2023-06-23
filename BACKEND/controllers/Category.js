bc1c9a388ba3d11f4c8246efe02f977f2ccfaae7
Robinsidhu11 <rbnsidhu11@gmail.com>
2023-06-23 16:56:22 +0530

    category page controller done, pending: top selling courses part

--- BACKEND/controllers/Category.js
+++ BACKEND/controllers/Category.js
@@ -1,4 +1,5 @@
 const Category=require('../models/Category')
+const Course = require('../models/Course')
 
 //to create a category (only for admin to access)
 exports.createCategory=async (req,res)=>{
@@ -51,6 +52,52 @@ exports.getAllCategories=async (req,res)=>{
         return res.status(500).json({
             success:false,
             message:"error in fetching all Categories",
+            error:err.message
+        })
+    }
+}
+
+//to show courses on catgeory page
+//say user looking for development courses,i will show those. and will also show the most selled courses or other categories courses too.
+// so lets create handler
+exports.categoryPageDetails=async (req,res)=>{
+    try{
+        //fetch category id
+        const {categoryid}=req.body
+
+        //find courses under that category id
+        const selectedCategory=await Category.findById({_id:categoryid}).populate("courses").exec()
+
+        //validation
+        if(!selectedCategory){
+            return res.status(400).json({
+                success:false,
+                message:"data not found for this category"
+            })
+        }
+
+        //find rest of the categories
+        //ne is not equal
+        const restOfTheCategories=await Category.find({_id:{$ne:categoryid}}).populate("courses").exec() 
+
+        //TODO: TOP SELLING COURSES, say top 10 courses
+        const topSellingCourses;
+        //return
+        return res.status(200).json({
+            success:true,
+            data:{
+                selectedCategory,
+                restOfTheCategories,
+                topSellingCourses
+            }
+            
+        })
+
+    }
+    catch(err){
+        return res.status(500).json({
+            success:false,
+            message:"cant fetched courses error",
             error:err.message
         })
     }
