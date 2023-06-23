fdbc381d4e63a180da9cb6959cd6ad3b28b59209
Robinsidhu11 <rbnsidhu11@gmail.com>
2023-06-23 13:37:38 +0530

    get course details with everything inside populated done in course.js

--- BACKEND/controllers/Course.js
+++ BACKEND/controllers/Course.js
@@ -4,7 +4,7 @@ const course=require('../models/Course')
 const User = require('../models/User')
 const Course = require('../models/Course')
 
-const createCourse=async (req,res)=>{
+exports.createCourse=async (req,res)=>{
     try{
         //fetch data
         const {courseName,courseDescription,whatYouWillLearn,price,category}=req.body
@@ -90,7 +90,7 @@ const createCourse=async (req,res)=>{
     }
 }
 
-const showAllCourses=async (req,res)=>{
+exports.showAllCourses=async (req,res)=>{
     try{
         const allCourses=await Course.find({})
 
@@ -103,6 +103,44 @@ const showAllCourses=async (req,res)=>{
         return res.status(500).json({
             success:false,
             message:"cant fetch courses",
+            error:err.message
+        })
+    }
+}
+
+//get one course details (not ids of inner object. do populate everything inside that too)
+exports.getCourseDetails=async (req,res)=>{
+    try{
+        //get course id from req body
+        const {courseid}=req.body
+        //findcourse details
+        const CourseDetails=await Course.findById({_id:courseid}).populate(
+            { //populates the instruor in course and also additondetails inside that instructor's user model too
+                path:"instructor",
+                populate:{
+                    path:"additionalDetails"
+                }
+            }
+        ).populate("category")
+        .populate("ratingAndReviews")
+        .populate({
+            path:"courseContent",
+            populate:{
+                path:"subSection"
+            }
+        }).exec()
+
+        // casual validation
+        if(!CourseDetails){
+            return res.status(400).json({
+                success:false,
+                message:"course not found"
+            }) 
+        }
+    }
+    catch(err){
+        return res.status(500).json({
+            success:false,
             error:err.message
         })
     }
