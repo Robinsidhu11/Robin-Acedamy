2782177bf6c6cec29be799f765c2d034f4f64913
Robinsidhu11 <rbnsidhu11@gmail.com>
2023-06-24 15:43:23 +0530

    profile routes done

--- BACKEND/routes/Profile.js
+++ BACKEND/routes/Profile.js
@@ -6,4 +6,14 @@ const {authenticate}=require('../middlewares/auth')
 //importing all handlers of profil controller
 const {updateProfile,getAllUserDetails,updateDisplayPicture,getEnrolledCourses}=require('../controllers/Profile')
 const {deleteAccount}=require('../controllers/DeleteAccount')
+
+// Delete User Account
+profileRoutes.delete("/deleteProfile",deleteAccount)
+
+profileRoutes.put("/updateProfile",authenticate,updateProfile)
+profileRoutes.put("/updateDisplayProfile",authenticate,updateDisplayPicture)
+
+profileRoutes.get("/getUserDetails",authenticate,getAllUserDetails)
+profileRoutes.get("/getEnrolledCourses",authenticate,getEnrolledCourses)
+
 module.exports=profileRoutes
